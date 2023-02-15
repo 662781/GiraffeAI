ae0852df2d84d754a6502214bc351faeaa2ca648
LightningLion_Student <662781@student.inholland.nl>
2023-02-15 09:57:41 +0100

    Tried something with Mediapipe and Python Virtual Environments :sweat:

---
+++ index.py
@@ -0,0 +1,27 @@
+import cv2
+import mediapipe as mp
+import time
+
+videoCapture = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+previousTime = 0
+currentTime = 0
+while True:
+    succes, img = videoCapture.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
+
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id, lm)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    currentTime = time.time()
+    fps = 1/(currentTime-previousTime)
+    previousTime = currentTime
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.imshow("webcam", img)
+    cv2.waitKey(1) 
