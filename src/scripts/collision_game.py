1688eb5639fdc42d82629305f838e0abd5f46c8f
Rascalov <vcs-github@mailbox.org>
2023-03-06 19:59:28 +0100

    Added full collision through polygon intersection

--- src/scripts/collision_game.py
+++ src/scripts/collision_game.py
@@ -4,36 +4,39 @@ import mediapipe as mp
 import time
 import random
 import math
+from shapely.geometry import LineString, Polygon
 
 
 # Camera en handen
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(min_detection_confidence=0.3, min_tracking_confidence=0.3)
+hands = mpHands.Hands(static_image_mode=True, max_num_hands=1)
 mpDraw = mp.solutions.drawing_utils
   
 #Houten plank
-wood = cv2.imread('resources/wood.png')
-size = 100
-wood = cv2.resize(wood, (60, 60), interpolation=cv2.INTER_LINEAR)
+wood = cv2.imread('resources/plank.jpg')
+size = 80
+wood = cv2.resize(wood, (size, size), interpolation=cv2.INTER_LINEAR)
 img2gray = cv2.cvtColor(wood, cv2.COLOR_BGR2GRAY)
 ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
 
 previousTime = 0
 currentTime = 0
-cap.set(3, 640)
-cap.set(4, 480)
+cap.set(3, 720)
+cap.set(4, 680)
 # Bereken random plaatsing van het hout met camera W en H
 width  = int(cap.get(3))  
 height = int(cap.get(4)) 
-woodX=random.randint(100, width-60)
-woodY=random.randint(100, height-60)
-
+woodX=random.randint(100, width-160)
+woodY=random.randint(100, height-160)
+woodLine = Polygon([(woodX, woodY), (woodX, woodY+size),(woodX +size, woodY), (woodX +size, woodY +size)  ])
 track_points = []
 track_lengths = []
 track_current = 0
 track_previous_point = 0,0
+spawn_time = time.time()
 
+score = 0
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -46,8 +49,7 @@ while True:
             
             # tracking, currently only usable for one hand. If we can get intersection detection, it will help a lot with collision detection
             px,py = track_previous_point
-            cx,cy = int(handLms.landmark[5].x * width), int(handLms.landmark[5].y *height )
-            #print("X: " + str(cx) + " Y: " + str(cy)) 
+            cx,cy = int(handLms.landmark[9].x * width), int(handLms.landmark[9].y *height )
             track_points.append([cx,cy])
             distance = math.hypot(cx-px, cy-py)
             track_lengths.append(distance)
@@ -56,19 +58,38 @@ while True:
 
             for i, point in enumerate(track_points):
                 if i != 0:
-                    cv2.line(frame, track_points[i-1], track_points[i], (0,0,255),20)
+                    cv2.line(frame, track_points[i-1], track_points[i], (0,0,255),5)
+                    line = LineString([track_points[i-1], track_points[i]])
+                    #woodLine = LineString([(woodX, woodY), (woodX, woodY+size)])
+                    
+                    if(line.intersects(woodLine)):
+                        print("Intersects!")
+                        
+                        track_points = []
+                        track_lengths = []
+                        woodX=random.randint(100, width-160)
+                        woodY=random.randint(100, height-160)
+                        woodLine = Polygon([(woodX, woodY), (woodX, woodY+size),(woodX +size, woodY), (woodX +size, woodY +size)])
+                        hitTime = (time.time() - spawn_time)
+                        print("Hit in " +  str(hitTime) + " seconds")
+                        score += int(100 - hitTime * 5)  
+                        spawn_time = time.time()
+
+            
+            #if X > woodX and X < (woodX+20) and  Y > woodY and Y <(woodY+40):
+            #            print("Wood has been hit")
             # --- todo: refactor into a method for
 
-            for id, lm in enumerate(handLms.landmark):
-                #print(id, lm)
-                X=lm.x*width
-                Y=lm.y*height
-                if X > woodX and X < (woodX+20) and  Y > woodY and Y <(woodY+40):
-                    print("Wood has been hit")
-                    track_points = []
-                    track_lengths = []
-                    woodX=random.randint(100, width-60)
-                    woodY=random.randint(100, height-60)
+            # for id, lm in enumerate(handLms.landmark):
+            #     #print(id, lm)
+            #     X=lm.x*width
+            #     Y=lm.y*height
+            #     if X > woodX and X < (woodX+20) and  Y > woodY and Y <(woodY+40):
+            #         #print("Wood has been hit")
+            #         track_points = []
+            #         track_lengths = []
+            #         woodX=random.randint(100, width-size)
+            #         woodY=random.randint(100, height-size)
                     #print(lm.x)
             #print(handLms.landmark[5].x)
             #mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
@@ -76,16 +97,18 @@ while True:
 
     # Region of Image https://stackoverflow.com/a/58177717/12464999 dit antwoord legt het best goed uit
     #roi = frame[-size-10:-10, -size-10:-10]
-    roi = frame[woodY:woodY+60,woodX:woodX+60]
+    roi = frame[woodY:woodY+size,woodX:woodX+size]
 
     # Set an index of where the mask is
     roi[np.where(mask)] = 0
     roi += wood
+    #cv2.line(frame, (woodX, woodY), (woodX, woodY+size), (0,255,0), 2)
+    cv2.rectangle(frame, (woodX, woodY), (woodX+size, woodY+size), (0,255,0), 2)
     
     currentTime = time.time()
     fps = 1/(currentTime-previousTime)
     previousTime = currentTime
-    cv2.putText(frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.putText(frame, "Score: " + str(score), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     cv2.imshow('WebCam', frame)
     if cv2.waitKey(1) == ord('q'):
