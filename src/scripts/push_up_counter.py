68947b6cd32558f75e77a53cecaa73a7f4f31a00
LightningLion_Student <662781@student.inholland.nl>
2023-03-19 14:34:22 +0100

    Updated Push-Up Counter
    - Now checks if the elbows are below the shoulders of the player

--- src/scripts/push_up_counter.py
+++ src/scripts/push_up_counter.py
@@ -4,15 +4,22 @@ import mediapipe as mp
 # Methods
 
 def does_push_up(results):
-     # Check if the person is doing a push-up
-        if results.pose_landmarks is not None:
-            left_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]
-            right_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]
-            left_shoulder = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER]
-            right_shoulder = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+    if results.pose_landmarks is not None:
+        # Get the landmarks for the hips, shoulders and elbows
+        left_hip = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_HIP]
+        right_hip = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.RIGHT_HIP]
+        left_shoulder = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_SHOULDER]
+        right_shoulder = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER]
+        left_elbow = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_ELBOW]
+        right_elbow = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.RIGHT_ELBOW]
 
-            if left_hip.y < left_shoulder.y and right_hip.y < right_shoulder.y:
+        # Check if the hips are above the shoulders
+        if left_hip.y < left_shoulder.y and right_hip.y < right_shoulder.y:
+            # Check if the elbows are below the shoulders
+            if left_elbow.y > left_shoulder.y and right_elbow.y > right_shoulder.y:
                 return True
+
+    return False
 
 # Main Script
 
