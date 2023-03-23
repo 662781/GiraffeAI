83678f62e8fcc341448d57ba9464224e3d603042
LightningLion_Student <662781@student.inholland.nl>
2023-03-23 19:58:16 +0100

    Progress :100:
    - Added new class PoseDetector
    - Added tensorflow_detection_test from a YT tutorial
    --> This one somehow makes my laptop make a really weird sound if I run it...
    - Minor changes

--- src/scripts/mini-games/warming-up/push_up_counter.py
+++ src/scripts/mini-games/warming-up/push_up_counter.py
@@ -1,27 +1,8 @@
+# Imports
 import cv2
 import mediapipe as mp
+from pose_detector import PoseDetector
 
-# Methods
-
-def does_push_up(results):
-    if results.pose_landmarks is not None:
-        # Get the landmarks for the hips, shoulders and elbows
-        left_hip = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_HIP]
-        right_hip = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.RIGHT_HIP]
-        left_shoulder = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_SHOULDER]
-        right_shoulder = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER]
-        left_elbow = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.LEFT_ELBOW]
-        right_elbow = results.pose_landmarks.landmark[mp.solutions.pose.PoseLandmark.RIGHT_ELBOW]
-
-        # Check if the hips are above the shoulders
-        if left_hip.y < left_shoulder.y and right_hip.y < right_shoulder.y:
-            # Check if the elbows are below the shoulders
-            if left_elbow.y > left_shoulder.y and right_elbow.y > right_shoulder.y:
-                return True
-
-    return False
-
-# Main Script
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -50,7 +31,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         cv2.putText(annotated_image, "Score: {}".format(push_up_count * 10), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
         # Check if the person is doing a push-up
-        if does_push_up(results):
+        if PoseDetector.does_push_up(results, mp):
             push_up_count += 1
 
         # Show the frame
