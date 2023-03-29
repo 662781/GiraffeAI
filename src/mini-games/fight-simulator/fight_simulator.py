2de89185ad4ab1341ced56d13741566e3c879349
MartinVeltman <martinveltman@live.nl>
2023-03-29 23:32:28 +0200

    added more punches, detection isnt accurate

--- src/mini-games/fight-simulator/fight_simulator.py
+++ src/mini-games/fight-simulator/fight_simulator.py
@@ -17,9 +17,13 @@ video_capture = cv2.VideoCapture(0)
 number_of_players = 1
 players = []
 
-# Initialize jab counter variable
+# Initialize punch counters
 number_of_jabs = 0
-stage = "hit"
+number_of_hooks = 0
+number_of_uppercuts = 0
+min_visiblity = 0.7
+
+stage = {"jab": "reset", "hook": "reset", "uppercut": "reset"}
 
 
 class Player:
@@ -43,8 +47,42 @@ class Player:
     left_foot_track_current = 0
     left_foot_track_previous_point = 0, 0
 
-    score = 0
+    score = {"jab": 0, "hook": 0, "uppercut": 0}
     spawn_time = time.time()
+
+
+def detect_punch(player, angle, stage, left_wrist_xy, left_elbow_xy, left_wrist_visibility, left_elbow_visibility):
+    # Calculate horizontal and vertical movement of the left wrist
+    wrist_dx = left_wrist_xy[0] - player.left_hand_track_previous_point[0]
+    wrist_dy = left_wrist_xy[1] - player.left_hand_track_previous_point[1]
+
+    # Detect jab
+    if angle > 120 and stage["jab"] == "reset" and left_wrist_visibility > 0.5 and left_elbow_visibility > 0.5 and abs(
+            wrist_dy) > abs(wrist_dx):
+        stage["jab"] = "stretched"
+    if angle < 20 and stage["jab"] == "stretched":
+        stage["jab"] = "reset"
+        player.score["jab"] += 1
+
+    # Detect hook
+    if 60 < angle < 130 and stage[
+        "hook"] == "reset" and left_wrist_visibility > 0.5 and left_elbow_visibility > 0.5 and abs(wrist_dx) > abs(
+        wrist_dy):
+        stage["hook"] = "hook angle"
+    if angle < 60 and stage["hook"] == "hook angle":
+        stage["hook"] = "reset"
+        player.score["hook"] += 1
+
+    # Detect uppercut
+    if angle > 150 and stage[
+        "uppercut"] == "reset" and left_wrist_visibility > 0.5 and left_elbow_visibility > 0.5 and wrist_dy > abs(
+        wrist_dx):
+        stage["uppercut"] = "stretched"
+    if angle < 90 and stage["uppercut"] == "stretched":
+        stage["uppercut"] = "reset"
+        player.score["uppercut"] += 1
+
+    return stage
 
 
 def calculate_angle(first_point, mid_point, end_point):
@@ -128,9 +166,10 @@ def create_players():
 
 
 def main_loop():
-    global number_of_jabs, stage
+    global stage
 
     create_players()
+    stage = {"jab": "reset", "hook": "reset", "uppercut": "reset"}
 
     while video_capture.isOpened():
         # Capture frame-by-frame
@@ -153,14 +192,28 @@ def main_loop():
             # Calculate the angle between the shoulder, elbow and wrist
             angle = calculate_angle(left_shoulder_xy, left_elbow_xy, left_wrist_xy)
 
-            # Update the stage and jab counter
-            stage, number_of_jabs = detect_jab(angle, stage, left_wrist_visibility,
-                                               left_elbow_visibility, number_of_jabs)
             for player in players:
                 track_left_hand(player, results.pose_landmarks.landmark, image.shape[1], image.shape[0])
 
+                # Update the stage and punch counters for each player
+                stage = detect_punch(player, angle, stage, left_wrist_xy, left_elbow_xy, left_wrist_visibility,
+                                     left_elbow_visibility)
+
             # Draw on the frame
             draw_on_frame(image, angle, left_elbow_xy, number_of_jabs, results)
+
+            # Display the punch counters on the image for the first player
+            player = players[0]
+            cv2.putText(image, f"Jabs: {player.score['jab']}", (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+            cv2.putText(image, f"Hooks: {player.score['hook']}", (10, 120), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+            cv2.putText(image, f"Uppercuts: {player.score['uppercut']}", (10, 170), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 255), 3)
+
+            # Convert the image back to BGR for display
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # Display the resulting image
+            cv2.imshow('Webcam', image)
 
         # Exit the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
